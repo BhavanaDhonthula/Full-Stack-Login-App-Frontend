fff613b6438a481123307364dd4a44b523d149b0
BhavanaDhonthula <225671061+BhavanaDhonthula@users.noreply.github.com>
2026-09-04 15:58:37 +0530

    Updated Backend fetch url

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -26,7 +26,7 @@ const Login = () => {
 
     const userDetails = { username, password };
 
-    const url = "http://localhost:5000/login";
+    const url = "https://full-stack-login-app-backend.onrender.com/login";
     const options = {
       method: "POST",
       headers: {
